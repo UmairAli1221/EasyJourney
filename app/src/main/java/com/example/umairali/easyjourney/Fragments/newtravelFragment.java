9218db7f021c7947d3a216f1502e7d58ad8dcf09
GH <umair.ali1221@gmail.com>
2017-03-02 07:57:14 -0800

    Implemment GeoFire

--- app/src/main/java/com/example/umairali/easyjourney/Fragments/newtravelFragment.java
+++ app/src/main/java/com/example/umairali/easyjourney/Fragments/newtravelFragment.java
@@ -3,6 +3,8 @@ package com.example.umairali.easyjourney.Fragments;
 import android.app.Fragment;
 import android.content.Context;
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -10,9 +12,22 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.umairali.easyjourney.Login;
 import com.example.umairali.easyjourney.MapsActivity;
 import com.example.umairali.easyjourney.R;
+import com.firebase.geofire.GeoFire;
+import com.firebase.geofire.GeoLocation;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by umair.ali on 11/2/2016.
@@ -23,7 +38,11 @@ public class newtravelFragment extends Fragment {
     Context context;
     Button btn;
     View myView;
-
+    String lt,lg;
+    private FirebaseAuth mAuth;
+    private FirebaseUser mFirebaseUser;
+    private DatabaseReference mDatabaseUsers;
+    private String mUserId;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -31,19 +50,51 @@ public class newtravelFragment extends Fragment {
         context = myView.getContext();
         time=(EditText)myView.findViewById(R.id.time);
         date=(EditText)myView.findViewById(R.id.date);
-        Startpoint=(EditText)myView.findViewById(R.id.spoint);
         EndPoint=(EditText)myView.findViewById(R.id.epoint);
         btn = (Button) myView.findViewById(R.id.searchbtn);
+        mAuth=FirebaseAuth.getInstance();
+        mDatabaseUsers= FirebaseDatabase.getInstance().getReference().child("Users");
+        mDatabaseUsers.keepSynced(true);
+        final GeoFire geoFire=new GeoFire(mDatabaseUsers);
+        mFirebaseUser = mAuth.getCurrentUser();
+        if (mFirebaseUser == null) {
+            Intent intent=new Intent(context, Login.class);
+            startActivity(intent);
+        } else {
+            mUserId = mFirebaseUser.getUid();
         btn.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(context, MapsActivity.class);
-                startActivity(i);
+                Startpoint = (EditText) myView.findViewById(R.id.spoint);
+                final String Startaddress = Startpoint.getText().toString();
+                Geocoder geocoder = new Geocoder(getActivity(), Locale.getDefault());
+                try {
+                    List addressList = geocoder.getFromLocationName(Startaddress, 1);
+                    if (addressList != null && addressList.size() > 0) {
+                        Address address = (Address) addressList.get(0);
+                        lt = String.valueOf(address.getLatitude());
+                        lg = String.valueOf(address.getLongitude());
+                        geoFire.setLocation(mUserId, new GeoLocation(address.getLatitude(), address.getLongitude())
+                                , new GeoFire.CompletionListener() {
+                                    @Override
+                                    public void onComplete(String key, DatabaseError error) {
+                                        if (error != null) {
+                                            Toast.makeText(context, "There was an error saving the location to GeoFire: ", Toast.LENGTH_LONG).show();
+                                        } else {
+                                            Intent i = new Intent(context, MapsActivity.class);
+                                            startActivity(i);
+                                            Toast.makeText(context, "\"Location saved on server successfully!\"", Toast.LENGTH_LONG).show();
+                                        }
+                                    }
+                                });
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-
-
         });
+        }
         return myView;
     }
 }
--- app/src/main/java/com/example/umairali/easyjourney/MapsActivity.java
+++ app/src/main/java/com/example/umairali/easyjourney/MapsActivity.java
@@ -1,18 +1,32 @@
 package com.example.umairali.easyjourney;
 
-import android.support.v4.app.FragmentActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.widget.Toast;
 
+import com.firebase.geofire.GeoFire;
+import com.firebase.geofire.GeoLocation;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    double lat=0.0,log=0.0;
+    private DatabaseReference mDatabaseUsers;
+    private FirebaseAuth mAuth;
+    private FirebaseUser mFirebaseUser;
+    private String mUserId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +36,45 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        mAuth=FirebaseAuth.getInstance();
+        mDatabaseUsers = FirebaseDatabase.getInstance().getReference().child("Users");
+        mDatabaseUsers.keepSynced(true);
+        mFirebaseUser = mAuth.getCurrentUser();
+        if (mFirebaseUser == null) {
+            Intent intent = new Intent(MapsActivity.this, Login.class);
+            startActivity(intent);
+        } else {
+            mUserId = mFirebaseUser.getUid();
+            GeoFire geoFire = new GeoFire(mDatabaseUsers);
+            geoFire.getLocation(mUserId, new com.firebase.geofire.LocationCallback() {
+                @Override
+                public void onLocationResult(String key, GeoLocation location) {
+                    if (location != null) {
+                        Toast.makeText(MapsActivity.this, "The location for key %s is [%f,%f]", Toast.LENGTH_LONG).show();
+                        // System.out.println(String.format("The location for key %s is [%f,%f]", key, location.latitude, location.longitude));
+                        lat = location.latitude;
+                        log = location.longitude;
+                        LatLng sydney = new LatLng(lat,log);
+                        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+                        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+                    } else {
+                        Toast.makeText(MapsActivity.this, "There is no location for key %s in GeoFire", Toast.LENGTH_LONG).show();
+                        // System.out.println(String.format("There is no location for key %s in GeoFire", key));
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    Toast.makeText(MapsActivity.this, "There was an error getting the GeoFire location:", Toast.LENGTH_LONG).show();
+                    //System.err.println("There was an error getting the GeoFire location: " + databaseError);
+                }
+            });
+        }
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
